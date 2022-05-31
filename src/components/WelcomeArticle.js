1f54835230090bed90044c9894cace003ea10d43
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-31 09:25:58 +0200

    add style

--- src/components/WelcomeArticle.js
+++ src/components/WelcomeArticle.js
@@ -1,6 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 const WelcomeArticle = (props) => {
+  const styles = {
+    article: css`
+      width: 800px;
+    `,
+  };
+
   return (
-    <article>
+    <article css={styles.article}>
       <h2>{props.heading}</h2>
       <p>Af {props.author}</p>
       <p>{props.body}</p>
